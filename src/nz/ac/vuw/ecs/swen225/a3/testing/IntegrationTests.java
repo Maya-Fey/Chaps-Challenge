ebb025f46bf862d9bcb042e7501aae05e487cc91
Fergus Currie <fergus.currie@gmail.com>
2019-09-23 13:39:19 +1200

    Added testing package

---
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/IntegrationTests.java
@@ -0,0 +1,21 @@
+package nz.ac.vuw.ecs.swen225.a3.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ferguscurrie
+ *
+ */
+class IntegrationTests {
+
+	/**
+	 * Basic test template with assert true
+	 */
+	@Test
+	void Test_1() {
+		assertTrue(true);
+	}
+
+}
