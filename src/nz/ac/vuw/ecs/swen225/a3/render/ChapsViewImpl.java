927a497f15cb7824e3bd89eff164e5010d8e3086
Jakob Coker <cokerjako@ecs.vuw.ac.nz>
2019-09-25 16:55:57 +1200

    Finished updateBoard method, can't test until we  can properly retrieve icons from visible interface

--- src/nz/ac/vuw/ecs/swen225/a3/render/ChapsViewImpl.java
+++ src/nz/ac/vuw/ecs/swen225/a3/render/ChapsViewImpl.java
@@ -1,13 +1,25 @@
 package nz.ac.vuw.ecs.swen225.a3.render;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.net.InterfaceAddress;
+import java.time.Year;
 
+import javax.imageio.IIOException;
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import nz.ac.vuw.ecs.swen225.a3.commons.Visible;
+import nz.ac.vuw.ecs.swen225.a3.commons.*;
 
 /**
  * An implementation for the ChapsView interface which represents the view for
@@ -22,33 +34,63 @@ public class ChapsViewImpl extends JPanel implements ChapsView {
 	/**
 	 * Fields
 	 */
+	private JPanel mazePanel = new JPanel();
 	private JLabel timeLabel = new JLabel();
 	private JLabel chipsLabel = new JLabel();
 	private JLabel tutorialMessage = new JLabel();
 	
+	private Visible visible;
+	
+	private Image border;
+	
+	private static int WIDTH = 9;
+	private static int HEIGHT = 9;
 	
 	/**
 	 * Constructor to initialize GUI
+	 * 
 	 */
 	public ChapsViewImpl() {
-		
 		super();
+
+		
 		this.setSize(new Dimension(600,600));
 		this.setLayout(new GridLayout());
 		this.add(chipsLabel);
 		this.add(timeLabel);
 		this.add(tutorialMessage);
+		this.add(mazePanel);
 		this.setVisible(true);
 		
-		
 	}
-
+	
 	
 
 	@Override
 	public void updateBoard(Visible[][] board) {
+		mazePanel = new JPanel();
+		mazePanel.setPreferredSize(new Dimension(700, 700));
+		mazePanel.setLayout(new GridLayout(WIDTH + 1, HEIGHT, 1, 1));
+		mazePanel.setBackground(Color.BLACK);
+		for(int x = 0; x < HEIGHT; x++) {
+			for(int y = 0; y < WIDTH; y++) {
+				mazePanel.add(IconToLabel(board[x][y].getIcon())); 
+			}
+		}
+		
 		this.revalidate();
 		this.repaint();
+		
+	}
+	
+	/**
+	 * @param icon
+	 * @return iconLabel
+	 */
+	public JLabel IconToLabel(Icon icon) {
+		JLabel iconLabel = new JLabel();
+		iconLabel.setIcon(icon);
+		return iconLabel;
 		
 	}
 
