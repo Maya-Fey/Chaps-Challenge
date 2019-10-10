45ed63a299e98de3af11f0fccfa00184401612d1
Fergus Currie <fergus.currie@gmail.com>
2019-10-11 12:36:07 +1300

    Created a JUnit test for testing the exit class, this is in the TestingMaze file

--- src/nz/ac/vuw/ecs/swen225/a3/testing/Testing.java
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/Testing.java
@@ -2,17 +2,32 @@ package nz.ac.vuw.ecs.swen225.a3.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.event.KeyEvent;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+
 import org.junit.jupiter.api.Test;
 
+import nz.ac.vuw.ecs.swen225.a3.application.ChapsController;
+import nz.ac.vuw.ecs.swen225.a3.application.ChapsControllerFactory;
+import nz.ac.vuw.ecs.swen225.a3.application.ChapsControllerImpl;
 import nz.ac.vuw.ecs.swen225.a3.application.Main;
+import nz.ac.vuw.ecs.swen225.a3.commons.IconFactory;
+import nz.ac.vuw.ecs.swen225.a3.maze.ChapsAction;
+import nz.ac.vuw.ecs.swen225.a3.maze.ChapsModelFactory;
+import nz.ac.vuw.ecs.swen225.a3.maze.Exit;
+import nz.ac.vuw.ecs.swen225.a3.render.ChapsViewFactory;
 import nz.ac.vuw.ecs.swen225.a3.render.ChapsViewImpl;
+import nz.ac.vuw.ecs.swen225.a3.maze.Position;
 
 /**
  * JUnit test cases for testing application package
  * @author ferguscurrie
  *
  */
-class ApplicationTesting {
+class Testing {
 	
 	
 
@@ -45,18 +60,8 @@ class ApplicationTesting {
 
 	}
 	
-	/**
-	 * Test the update board method of ChapsViewImpl works 
-	 */
-	@Test
-	void test_update_board() {
-		/*
-		ChapsViewImpl cvl = new ChapsViewImpl();
-		System.out.println(cvl.);
-		*/
-		assertTrue(true);
-
-	}
+	
+	
 
 
 }
--- src/nz/ac/vuw/ecs/swen225/a3/testing/TestingMaze.java
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/TestingMaze.java
@@ -2,7 +2,13 @@ package nz.ac.vuw.ecs.swen225.a3.testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.swing.ImageIcon;
+
 import org.junit.jupiter.api.Test;
+
+import nz.ac.vuw.ecs.swen225.a3.commons.IconFactory;
+import nz.ac.vuw.ecs.swen225.a3.maze.Exit;
+import nz.ac.vuw.ecs.swen225.a3.maze.Position;
 
 /**
  * JUnit test cases for maze package, basic functionalities of game 
@@ -12,12 +18,65 @@ import org.junit.jupiter.api.Test;
 class TestingMaze {
 
 	/**
-	 * Default 
+	 * Tests the exit class
 	 */
 	@Test
-	void test_default() {
-		fail("Not yet implemented");
+	void test_exit() {
+		//Constructing 
+		Exit e = null;
+		ImageIcon i = null;
+		try {
+			i = (ImageIcon) IconFactory.INSTANCE.loadIcon("dirt.png");
+			Position p = new Position(0,0);
+			e = new Exit(p,"",i);
+		}catch(Error ee) {
+			assert(false);
+		}
+		assert(true);
+		//Clone
+		Exit e2 = e.clone();
+		assertTrue(e2 != e);
+		assertTrue(e2.getName().equals(e.getName()));
+		assertTrue(e2.getPosition().x == (e.getPosition().x) && e2.getPosition().y == (e.getPosition().y));
+		//Set position
+		Position pp = new Position(0,1);
+		e.setPosition(pp);
+		assertTrue(e.getPosition().y == 1);
+		//Other
+		assertTrue(e.isFloor() == false);
+		assertTrue(e.getIcon() == i);
 	}
+	
+	/**
+	 * Tests if an the exit class 
+	 */
+	@Test
+	void test_exit() {
+		//Constructing 
+		Exit e = null;
+		ImageIcon i = null;
+		try {
+			i = (ImageIcon) IconFactory.INSTANCE.loadIcon("dirt.png");
+			Position p = new Position(0,0);
+			e = new Exit(p,"",i);
+		}catch(Error ee) {
+			assert(false);
+		}
+		assert(true);
+		//Clone
+		Exit e2 = e.clone();
+		assertTrue(e2 != e);
+		assertTrue(e2.getName().equals(e.getName()));
+		assertTrue(e2.getPosition().x == (e.getPosition().x) && e2.getPosition().y == (e.getPosition().y));
+		//Set position
+		Position pp = new Position(0,1);
+		e.setPosition(pp);
+		assertTrue(e.getPosition().y == 1);
+		//Other
+		assertTrue(e.isFloor() == false);
+		assertTrue(e.getIcon() == i);
+	}
+	
 	
 
 }
