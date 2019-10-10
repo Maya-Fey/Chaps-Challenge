50da378852e1881f2777988a6697d7443d678362
Fergus Currie <fergus.currie@gmail.com>
2019-10-10 16:39:24 +1300

    Commiting test of method four_digit_format in ChapsViewImpl

--- src/nz/ac/vuw/ecs/swen225/a3/testing/ApplicationTesting.java
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/ApplicationTesting.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import nz.ac.vuw.ecs.swen225.a3.application.Main;
+import nz.ac.vuw.ecs.swen225.a3.render.ChapsViewImpl;
 
 /**
  * JUnit test cases for testing application package
@@ -14,6 +15,7 @@ import nz.ac.vuw.ecs.swen225.a3.application.Main;
 class ApplicationTesting {
 	
 	
+
 	/**
 	 * Try run main, make sure no error occurs 
 	 */
@@ -28,5 +30,33 @@ class ApplicationTesting {
 		assertTrue(true);
 
 	}
+	
+	
+	/**
+	 * Test the four digit format method of ChapsViewImpl works correctly 
+	 */
+	@Test
+	void test_four_digit_format() {
+		int x = 4;
+		String expected = "0004";
+		
+		String result = new ChapsViewImpl().fourDigitFormat(x);
+		assertTrue(result.equals(expected));
+
+	}
+	
+	/**
+	 * Test the update board method of ChapsViewImpl works 
+	 */
+	@Test
+	void test_update_board() {
+		/*
+		ChapsViewImpl cvl = new ChapsViewImpl();
+		System.out.println(cvl.);
+		*/
+		assertTrue(true);
+
+	}
+
 
 }
