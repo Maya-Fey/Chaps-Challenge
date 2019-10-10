93d948b53a48b2f26062c0e84b1f51838fc0d6a3
Fergus Currie <fergus.currie@gmail.com>
2019-10-10 15:46:55 +1300

    Testing IconFactory class, tests: cache, making and composite. Also added file for testing application

---
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/ApplicationTesting.java
@@ -0,0 +1,23 @@
+package nz.ac.vuw.ecs.swen225.a3.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * JUnit test cases for testing application package
+ * @author ferguscurrie
+ *
+ */
+class ApplicationTesting {
+	
+	
+	/**
+	 * t
+	 */
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/TestingMaze.java
@@ -0,0 +1,23 @@
+package nz.ac.vuw.ecs.swen225.a3.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * JUnit test cases for maze package, basic functionalities of game 
+ * @author ferguscurrie
+ *
+ */
+class TestingMaze {
+
+	/**
+	 * Default 
+	 */
+	@Test
+	void test_default() {
+		fail("Not yet implemented");
+	}
+	
+
+}
