9557a4213b888b1d0d3582ef6dd591077b8fae71
Claire <foxcoindev@gmail.com>
2019-10-11 23:48:05 +1300

    Added more plugin tests

--- src/nz/ac/vuw/ecs/swen225/a3/testing/PluginTests.java
+++ src/nz/ac/vuw/ecs/swen225/a3/testing/PluginTests.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import nz.ac.vuw.ecs.swen225.a3.maze.InteractableExitLock;
 import nz.ac.vuw.ecs.swen225.a3.maze.InteractableItem;
 import nz.ac.vuw.ecs.swen225.a3.maze.TileFree;
 import nz.ac.vuw.ecs.swen225.a3.plugin.EditJsonDialog;
 import nz.ac.vuw.ecs.swen225.a3.plugin.LevelBuilderFrame;
+import nz.ac.vuw.ecs.swen225.a3.plugin.LevelBuilderModel;
 
 /**
  * @author Claire
@@ -52,6 +54,27 @@ public class PluginTests {
 	void testSomethingEditable()
 	{
 		assertTrue(EditJsonDialog.canEdit(new InteractableItem().persist()));
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	void testExportFunctions()
+	{
+		LevelBuilderModel model = new LevelBuilderModel();
+		model = new LevelBuilderModel(model.export());
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	void testAddInteractable()
+	{
+		LevelBuilderModel model = new LevelBuilderModel();
+		model.addInteractable(new InteractableExitLock(), 5, 5);
+		assertTrue(model.export().getInteractables().get(0).getClass() == InteractableExitLock.class);
 	}
 
 	
